ff41287b82d3014c1f1b84174e30228b87f29fbf
Ardhana-Hidayat <ardhanahidayat61@gmail.com>
2025-04-28 19:38:45 +0700

    Add Tailwind for Dashboard & Login

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+export default {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#3B378B',   
+          hover: '#524CC3',
+        },
+      },
+    },
+    plugins: [],
+  }
+  
